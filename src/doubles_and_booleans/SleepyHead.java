eeb8c75c6574ebf7927e9f82333ad811f8019510
aahmed2006 <league@WTS22.attlocal.net>
2018-12-29 17:26:42 -0800

    SleepyHead, StringMethods1, PiAloud

--- src/doubles_and_booleans/SleepyHead.java
+++ src/doubles_and_booleans/SleepyHead.java
@@ -1,4 +1,7 @@
 package doubles_and_booleans;
+
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 
 //    Level 0
@@ -16,7 +19,27 @@ public class SleepyHead {
 		 * JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head",
 		 * JOptionPane.YES_NO_OPTION);
 		 */
-
+		int weekday = JOptionPane.showConfirmDialog(null, "Is it a weekday?");
+		if (weekday == 0) {
+			isWeekday = true;
+		} else {
+			isWeekday = false;
+		}
+		int vacation = JOptionPane.showConfirmDialog(null, "Is it a vacation?");
+		if (vacation == 0) {
+			isVacation = true;
+		} else {
+			isVacation = false;
+		}
+		if (isVacation == true || isWeekday == false) {
+			System.out.println("Sleep in.");
+		}
+		if (isWeekday == true) {
+			System.out.println("Get up lazybones!");
+		}
+		if (isWeekday == true && isVacation == true) {
+			System.out.println("Sleep in.");
+		}
 		/*
 		 * Print “sleep in�? if it is a vacation or a weekend. If it’s a weekday, print
 		 * “get up lazybones!�? If it is a weekday, and we are on vacation, print “sleep
