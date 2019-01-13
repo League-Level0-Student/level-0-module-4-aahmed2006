7e9606baf9487c7bf979279a900f977e26c7ee26
aahmed2006 <league@WTS22.attlocal.net>
2019-01-12 17:25:53 -0800

    HappyPet, OwnAdventure

--- src/charAt_scanners_optionDialogs/OwnAdventure.java
+++ src/charAt_scanners_optionDialogs/OwnAdventure.java
@@ -5,6 +5,7 @@ import javax.swing.JOptionPane;
 public class OwnAdventure {
 	public static void main(String[] args) {
 		JOptionPane.showMessageDialog(null, "This is a story where you can decide the path of the plot.");
-
-	}
+		JOptionPane.showMessageDialog(null, "You are at a farm. You see some animals.");
+		JOptionPane.showOptionDialog(null, "Which animal would you like to milk", "story", 0, JOptionPane.INFORMATION_MESSAGE, icon, options, initialValue)
+}
 }
